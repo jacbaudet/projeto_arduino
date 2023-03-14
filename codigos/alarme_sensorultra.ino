cf44e94e9eeb5eced1ba69c305a69bc1c58871d9
Jacqueline Baudet <42481870+jacbaudet@users.noreply.github.com>
2023-03-14 09:40:45 -0300

    Add files via upload

---
+++ codigos/alarme_sensorultra.ino
@@ -0,0 +1,37 @@
+// Projeto 15 - Sensor ultrasônico básico
+
+#define trigPin 12
+#define echoPin 13
+
+
+void setup() {
+Serial.begin (9600);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+
+}
+
+void loop() {
+long duration, distance;
+// Configura o pino 12 com um pulso baixo "LOW" ou desligado ou ainda 0
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2); // Aguarda 2 microssegundos
+
+// Configura o pino 12 com pulso alto "HIGH" ou ligado ou ainda 1
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10); // Aguarda 10 microssegundos
+// Configura o pino 12 com pulso baixo novamente
+digitalWrite(trigPin, LOW);
+// PulseIn lê o tempo entre a chamada e o pino entrar em High
+duration = pulseIn(echoPin, HIGH);
+
+/*Esse calculo é baseado em s = v . t, lembrando que o tempo vem dobrado
+porque é o tempo de ida e volta do ultrassom */
+
+distance = ((duration/2) / 29.1);
+
+Serial.print(distance);
+Serial.println(" Cm");
+
+delay(500);
+}
